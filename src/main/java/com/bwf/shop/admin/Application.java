8141d75d24f5be1cf499096883ab601ee10c5334
titushan <titushan2010@163.com>
2020-09-09 15:05:50 +0800

    管理员 初始化项目 于 2020/09/09 15:04

---
+++ src/main/java/com/bwf/shop/admin/Application.java
@@ -0,0 +1,17 @@
+package com.bwf.shop.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.bwf.shop.admin.mapper")
+public class Application {
+
+    public static void main( String[] args ){
+
+        SpringApplication.run( Application.class , args );
+
+    }
+
+}
